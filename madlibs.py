43875d52300c4792b164161853d78f7145de679c
melridger <melrobiphone@gmail.com>
2022-06-27 16:43:39 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+# Madlibs project to show string concatenation using f string
+noun = input("Noun: ")
+friend = input("Name of friend: ")
+adj = input("Adjective: ")
+noun1 = input("Noun: ")
+
+madlib = f"Why do you spend money on {noun}? You could spend time with {friend}. \
+That could make you {adj} and you wouldn't need as much {noun1}."
+
+print(madlib)
